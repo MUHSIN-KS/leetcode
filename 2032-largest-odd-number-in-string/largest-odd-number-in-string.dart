3d4f47fafcf030f7935f97a5238b52929b25dab9
Muhammed  muhsin <muhsinmuhsin0368@gmail.com>
2025-10-16 10:27:14 +0530

    Time: 4 ms (50.00%) | Memory: 153.5 MB (10.00%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.dart
@@ -0,0 +1,11 @@
+class Solution {
+  String largestOddNumber(String num) {
+  for (int i = num.length - 1; i >= 0; i--) {
+    int digit = int.parse(num[i]);
+    if (digit % 2 != 0) {
+      return num.substring(0, i + 1); 
+    }
+  }
+  return ""; 
+}
+}
