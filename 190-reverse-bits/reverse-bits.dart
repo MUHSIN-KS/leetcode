5d57953b6085d1122b03e28756ca8be0a1eccabd
Muhammed  muhsin <muhsinmuhsin0368@gmail.com>
2026-02-24 16:41:55 +0530

    Time: 384 ms (17.65%) | Memory: 148.3 MB (77.65%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.dart
@@ -0,0 +1,8 @@
+class Solution {
+  int reverseBits(int n) {
+    String binary = n.toRadixString(2);
+    binary = binary.padLeft(32, '0');
+    String reversed = binary.split('').reversed.join();
+    return int.parse(reversed, radix: 2);
+  }
+}
