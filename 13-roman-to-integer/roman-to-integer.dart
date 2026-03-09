4d68fadb1301a87decf1ecb0f68e6b08e892890b
Muhammed  muhsin <muhsinmuhsin0368@gmail.com>
2026-03-09 10:53:04 +0530

    Time: 15 ms (31.67%) | Memory: 148.3 MB (92.00%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.dart
@@ -0,0 +1,23 @@
+class Solution {
+  int romanToInt(String s) {
+    Map<String, int> roman = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+    int total = 0;
+    for (int i = 0; i < s.length; i++) {
+      int value = roman[s[i]]!;
+      if (i < s.length - 1 && value < roman[s[i + 1]]!) {
+        total -= value;
+      } else {
+        total += value;
+      }
+    }
+    return total;
+  }
+}
