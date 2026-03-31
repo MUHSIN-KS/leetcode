29cd8384b0042d854113dc7f7f0084f2ed6bb3ec
Muhammed  muhsin <muhsinmuhsin0368@gmail.com>
2026-03-31 08:46:29 +0530

    Time: 1 ms (41.67%) | Memory: 149.2 MB (33.33%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.dart
@@ -0,0 +1,9 @@
+class Solution {
+  int titleToNumber(String columnTitle) {
+  int result = 0;
+  for (int i= 0;i<columnTitle.length;i++) {
+    result = result * 26+(columnTitle.codeUnitAt(i)-'A'.codeUnitAt(0)+1);
+  }
+  return result;
+}
+}
